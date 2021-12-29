2f24b14f547938fb018a15f3b851c7639211e837
Ashutosh Kumbhar <60011389+2727-ask@users.noreply.github.com>
2021-12-29 22:40:27 +0530

    Update fetchData.py

--- fetchData.py
+++ fetchData.py
@@ -1,16 +1,14 @@
-from fastapi import FastAPI
-import requests
-from datetime import datetime
-import requests
 import json
+from datetime import datetime
 
+import requests
+from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get('/fetchReel/{id}')
-
 def fetchReel(id):
-
     link = 'https://www.instagram.com/accounts/login/'
     login_url = 'https://www.instagram.com/accounts/login/ajax/'
 
@@ -36,3 +34,21 @@ def fetchReel(id):
     json_data = json.loads(login_response.text)
 
     print(json_data)
+    try:
+        from urllib.parse import urlparse
+    except ImportError:
+        import urlparse
+
+    BASE_URL = 'https://www.instagram.com/'
+    CHROME_WIN_UA = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
+
+    session = requests.Session()
+    session.headers = {'user-agent': CHROME_WIN_UA, 'Referer': BASE_URL}
+    session.cookies.set('ig_pr', '1')
+    req = session.get(BASE_URL)
+    session.headers.update({'X-CSRFToken': req.cookies['csrftoken']})
+
+    url = f"https://www.instagram.com/reel/{id}/?__a=1"
+    response = session.get(url, cookies="", headers={'Host': urlparse(url).hostname}, stream=False, timeout=90)
+
+    return (response.content)
