08c4d68622748a5db0b53b207050cd2d7ecbacfd
Ashutosh Kumbhar <60011389+2727-ask@users.noreply.github.com>
2021-12-29 22:33:19 +0530

    Update fetchData.py

--- fetchData.py
+++ fetchData.py
@@ -4,11 +4,13 @@ from datetime import datetime
 import requests
 import json
 
+
 app = FastAPI()
 
 @app.get('/fetchReel/{id}')
 
 def fetchReel(id):
+
     link = 'https://www.instagram.com/accounts/login/'
     login_url = 'https://www.instagram.com/accounts/login/ajax/'
 
@@ -34,7 +36,6 @@ def fetchReel(id):
     json_data = json.loads(login_response.text)
 
     if json_data["authenticated"]:
-
         print("login successful")
         cookies = login_response.cookies
         cookie_jar = cookies.get_dict()
@@ -42,27 +43,6 @@ def fetchReel(id):
         print("csrf_token: ", csrf_token)
         session_id = cookie_jar['sessionid']
         print("session_id: ", session_id)
-
-        try:
-            from urllib.parse import urlparse
-        except ImportError:
-            import urlparse
-
-        BASE_URL = 'https://www.instagram.com/'
-        CHROME_WIN_UA = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
-
-        session = requests.Session()
-        session.headers = {'user-agent': CHROME_WIN_UA, 'Referer': BASE_URL}
-        session.cookies.set('ig_pr', '1')
-        req = session.get(BASE_URL)
-        session.headers.update({'X-CSRFToken': req.cookies['csrftoken']})
-
-        url = f"https://www.instagram.com/reel/{id}/?__a=1"
-        response = session.get(url, cookies="", headers={'Host': urlparse(url).hostname}, stream=False, timeout=90)
-
-        return (response.content)
-
-
     else:
         print("login failed ", login_response.text)
 
