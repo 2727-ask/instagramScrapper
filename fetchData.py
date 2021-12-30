6ab505fdc62814b82b5ae416dd52dbc86def2283
Ashutosh Kumbhar <60011389+2727-ask@users.noreply.github.com>
2021-12-30 10:21:53 +0530

    Update fetchData.py

--- fetchData.py
+++ fetchData.py
@@ -16,39 +16,21 @@ def fetchReel(id):
     response = requests.get(link)
     csrf = response.cookies['csrftoken']
 
+    s = requests.Session()
     payload = {
         'username': 'ashutoshkumbhar27',
         'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:{time}:Asdfghjkl@27',
         'queryParams': {},
         'optIntoOneTap': 'false'
     }
-
     login_header = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
         "X-Requested-With": "XMLHttpRequest",
         "Referer": "https://www.instagram.com/accounts/login/",
         "x-csrftoken": csrf
     }
+    res = s.post(login_url,data=payload,headers=login_header)
+    print(res.content)
+    return("Hello World")
 
-    login_response = requests.post(login_url, data=payload, headers=login_header)
-    json_data = json.loads(login_response.text)
 
-    print(json_data)
-    try:
-        from urllib.parse import urlparse
-    except ImportError:
-        import urlparse
-
-    BASE_URL = 'https://www.instagram.com/'
-    CHROME_WIN_UA = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
-
-    session = requests.Session()
-    session.headers = {'user-agent': CHROME_WIN_UA, 'Referer': BASE_URL}
-    session.cookies.set('ig_pr', '1')
-    req = session.get(BASE_URL)
-    session.headers.update({'X-CSRFToken': req.cookies['csrftoken']})
-
-    url = f"https://www.instagram.com/reel/{id}/?__a=1"
-    response = session.get(url, cookies="", headers={'Host': urlparse(url).hostname}, stream=False, timeout=90)
-
-    return (response.content)
