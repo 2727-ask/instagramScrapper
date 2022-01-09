657c57b093c86c440fd69c620b584f23e8e2cc0c
Ashutosh Kumbhar <60011389+2727-ask@users.noreply.github.com>
2022-01-09 15:04:53 +0530

    Update fetchData.py

--- fetchData.py
+++ fetchData.py
@@ -1,24 +1,26 @@
-import json
-from datetime import datetime
-
-import requests
 from fastapi import FastAPI
+import requests
+from datetime import datetime
+import requests
+import json
 
 app = FastAPI()
 
+isLoggedIn = False
+s = requests.Session()
 
-@app.get('/fetchReel/{id}')
-def fetchReel(id):
+
+def authenticateSesssion():
     link = 'https://www.instagram.com/accounts/login/'
     login_url = 'https://www.instagram.com/accounts/login/ajax/'
 
     time = int(datetime.now().timestamp())
+    print(time)
     response = requests.get(link)
     csrf = response.cookies['csrftoken']
-
-    s = requests.Session()
+    print("CSRF TOKEN", csrf);
     payload = {
-        'username': 'ashutoshkumbhar27',
+        'username': 'carrylinati',
         'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:{time}:Asdfghjkl@27',
         'queryParams': {},
         'optIntoOneTap': 'false'
@@ -27,9 +29,33 @@ def fetchReel(id):
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
         "X-Requested-With": "XMLHttpRequest",
         "Referer": "https://www.instagram.com/accounts/login/",
-        "x-csrftoken": csrf
+        "x-csrftoken": "2T1msXxiNHNgpiVAANwtzrNB5IsrR1pT"
     }
-    res = s.post(login_url, data=payload, headers=login_he
-    url = "https://www.instagram.com/reel/"+id+"/?__a=1"
-    result = s.get(url)
-    return(json.loads(result.content))
+
+    res = s.post(login_url, data=payload, headers=login_header)
+    print(res.content)
+
+    jsonData = json.loads(res.content)
+    if (jsonData["authenticated"]):
+        isLoggedIn = jsonData["authenticated"]
+        return isLoggedIn
+    else:
+        return False
+
+
+
+@app.get('/makeReq/{id}')
+def makeReq(id):
+    global isLoggedIn
+    if(isLoggedIn == False):
+        isLoggedIn = authenticateSesssion()
+        return {
+            'status':'again'
+        }
+    else:
+        print("Authenticated")
+        data = s.get(f"https://www.instagram.com/p/{id}/?__a=1")
+        print("Data", data.content)
+        return json.loads(data.content)
+
+
