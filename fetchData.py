e40c13ee5519d128e6a58329310480a3c4008cc5
Ashutosh Kumbhar <60011389+2727-ask@users.noreply.github.com>
2022-01-10 14:52:31 +0530

    Update fetchData.py

--- fetchData.py
+++ fetchData.py
@@ -1,61 +1,57 @@
-from fastapi import FastAPI
-import requests
 from datetime import datetime
+
 import requests
+from fastapi import FastAPI
 import json
 
 app = FastAPI()
 
-isLoggedIn = False
-s = requests.Session()
 
+class AuthSession:
+    s = requests.Session()
+    isLoggedIn = False
+    def authenticate(self):
+        link = 'https://www.instagram.com/accounts/login/'
+        login_url = 'https://www.instagram.com/accounts/login/ajax/'
 
-def authenticateSesssion():
-    link = 'https://www.instagram.com/accounts/login/'
-    login_url = 'https://www.instagram.com/accounts/login/ajax/'
+        time = int(datetime.now().timestamp())
+        print(time)
+        response = requests.get(link)
+        csrf = response.cookies['csrftoken']
+        print("CSRF TOKEN", csrf);
+        payload = {
+            'username': 'carrylinati',
+            'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:{time}:Asdfghjkl@27',
+            'queryParams': {},
+            'optIntoOneTap': 'false'
+        }
+        login_header = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
+            "X-Requested-With": "XMLHttpRequest",
+            "Referer": "https://www.instagram.com/accounts/login/",
+            "x-csrftoken": "2T1msXxiNHNgpiVAANwtzrNB5IsrR1pT"
+        }
 
-    time = int(datetime.now().timestamp())
-    print(time)
-    response = requests.get(link)
-    csrf = response.cookies['csrftoken']
-    print("CSRF TOKEN", csrf);
-    payload = {
-        'username': 'carrylinati',
-        'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:{time}:Asdfghjkl@27',
-        'queryParams': {},
-        'optIntoOneTap': 'false'
-    }
-    login_header = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
-        "X-Requested-With": "XMLHttpRequest",
-        "Referer": "https://www.instagram.com/accounts/login/",
-        "x-csrftoken": "2T1msXxiNHNgpiVAANwtzrNB5IsrR1pT"
-    }
+        res = AuthSession.s.post(login_url, data=payload, headers=login_header)
+        print(res.content)
 
-    res = s.post(login_url, data=payload, headers=login_header)
-    print(res.content)
+        jsonData = json.loads(res.content)
+        if (jsonData["authenticated"]):
+            AuthSession.isLoggedIn = True
+        else:
+            AuthSession.isLoggedIn = False
 
-    jsonData = json.loads(res.content)
-    if (jsonData["authenticated"]):
-        isLoggedIn = jsonData["authenticated"]
-        return isLoggedIn
-    else:
-        return False
-
+    def fetchReel(self,id):
+        res = AuthSession.s.get(f"https://www.instagram.com/p/{id}/?__a=1")
+        return json.loads(res.content)
 
 
 @app.get('/makeReq/{id}')
 def makeReq(id):
-    global isLoggedIn
-    if(isLoggedIn == False):
-        isLoggedIn = authenticateSesssion()
-        return {
-            'status':'again'
-        }
+    session = AuthSession()
+    if(session.isLoggedIn == False):
+        print("Authenticating")
+        session.authenticate()
     else:
-        print("Authenticated")
-        data = s.get(f"https://www.instagram.com/p/{id}/?__a=1")
-        print("Data", data.content)
-        return json.loads(data.content)
-
-
+        print("Already Authenticated")
+        return session.fetchReel(id)
